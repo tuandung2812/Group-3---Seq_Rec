43bddb227e838d66fdf2b104bb03b1a33247f064
tuandung2812 <dung.nt194427@sis.hust.edu.vn>
2023-02-05 06:33:45 +0700

    Alter gpu support

--- src/run_augment_item_mask.sh
+++ src/run_augment_item_mask.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -x
 
 # Run SASREC with full data
-python main.py --model_name SASRec --epoch 200 --early_stopping 20 --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Video_Games' --augment 'None' --train_ratio 1 --augment "item_mask" --gpu 0
+python main.py --model_name SASRec --epoch 200 --early_stopping 20 --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Video_Games' --augment 'None' --train_ratio 1 --augment "item_mask" --gpu '0'
 
 # Run SASRec with 0.1 data
-python main.py --model_name SASRec --epoch 200 --early_stopping 20 --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Video_Games' --augment 'None' --train_ratio 0.1 --augment "item_mask" --gpu 0
+python main.py --model_name SASRec --epoch 200 --early_stopping 20 --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Video_Games' --augment 'None' --train_ratio 0.1 --augment "item_mask" --gpu '0'
 
 # Run SASRec with 0.25 data
-python main.py --model_name SASRec --epoch 200 --early_stopping 20 --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Video_Games' --augment 'None' --train_ratio 0.25 --augment "item_mask" --gpu 0
+python main.py --model_name SASRec --epoch 200 --early_stopping 20 --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Video_Games' --augment 'None' --train_ratio 0.25 --augment "item_mask" --gpu '0'
 
 # Run SASRec with 0.5 data
-python main.py --model_name SASRec --epoch 200 --early_stopping 20 --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Video_Games' --augment 'None' --train_ratio 0.5 --augment "item_mask" --gpu 0
+python main.py --model_name SASRec --epoch 200 --early_stopping 20 --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Video_Games' --augment 'None' --train_ratio 0.5 --augment "item_mask" --gpu '0'
 
--- src/run_caser.sh
+++ src/run_caser.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -x
 
 # Run Caser with full data
-python main.py --model_name Caser --epoch 200 --early_stopping 20 --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Video_Games' --augment 'None' --train_ratio 1 --gpu 0
+python main.py --model_name Caser --epoch 200 --early_stopping 20 --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Video_Games' --augment 'None' --train_ratio 1 --gpu '0'
 
 # Run Caser with 0.1 data
-python main.py --model_name Caser --epoch 200 --early_stopping 20 --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Video_Games' --augment 'None' --train_ratio 0.1 --gpu 0
+python main.py --model_name Caser --epoch 200 --early_stopping 20 --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Video_Games' --augment 'None' --train_ratio 0.1 --gpu '0'
 
 # Run Caser with 0.25 data
-python main.py --model_name Caser --epoch 200 --early_stopping 20 --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Video_Games' --augment 'None' --train_ratio 0.25 --gpu 0
+python main.py --model_name Caser --epoch 200 --early_stopping 20 --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Video_Games' --augment 'None' --train_ratio 0.25 --gpu '0'
 
 # Run Caser with 0.5 data
-python main.py --model_name Caser --epoch 200 --early_stopping 20 --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Video_Games' --augment 'None' --train_ratio 0.5 --gpu 0
+python main.py --model_name Caser --epoch 200 --early_stopping 20 --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Video_Games' --augment 'None' --train_ratio 0.5 --gpu '0'
 
