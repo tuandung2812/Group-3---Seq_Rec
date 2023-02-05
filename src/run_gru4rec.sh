9fc8e57ca936d55d7bfa9a58a10f4adb0d909d41
tuandung2812 <dung.nt194427@sis.hust.edu.vn>
2023-02-05 15:52:56 +0700

    Modify bash file

--- src/run_gru4rec.sh
+++ src/run_gru4rec.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -x
 
 # Run GRU4REC with full data
-python main.py --model_name GRU4REC --epoch 200 --early_stopping 20 --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Video_Games' --augment 'None' --train_ratio 1 --gpu '0'
+python main.py --model_name GRU4Rec --epoch 200 --early_stopping 20 --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Video_Games' --augment 'None' --train_ratio 1 --gpu '0'
 
 # Run GRU4REC with 0.1 data
-python main.py --model_name GRU4REC --epoch 200 --early_stopping 20 --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Video_Games' --augment 'None' --train_ratio 0.1 --gpu '0'
+python main.py --model_name GRU4Rec --epoch 200 --early_stopping 20 --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Video_Games' --augment 'None' --train_ratio 0.1 --gpu '0'
 
 # Run GRU4REC with 0.25 data
-python main.py --model_name GRU4REC --epoch 200 --early_stopping 20 --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Video_Games' --augment 'None' --train_ratio 0.25 --gpu '0'
+python main.py --model_name GRU4Rec --epoch 200 --early_stopping 20 --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Video_Games' --augment 'None' --train_ratio 0.25 --gpu '0'
 
 # Run GRU4REC with 0.5 data
-python main.py --model_name GRU4REC --epoch 200 --early_stopping 20 --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Video_Games' --augment 'None' --train_ratio 0.5 --gpu '0'
+python main.py --model_name GRU4Rec --epoch 200 --early_stopping 20 --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Video_Games' --augment 'None' --train_ratio 0.5 --gpu '0'
